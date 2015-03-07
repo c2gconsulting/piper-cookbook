49995f0c1f25155ae4cfba89107ad70735906596
Kachi Izukanne <cizukanne@c2gconsulting.com>
2015-03-07 22:36:39 +0100

    Initial commit

---
+++ mongodb-singlenode/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-singlenode"
+description 'MongoDB single node Berkshelf based install'
+maintainer  "Piper Labs"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
